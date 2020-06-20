822556816390330caca3cffa04ff0eb44e9ad720
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-06-20 12:20:07 +0200

    minor cleanup

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-type web-ext >/dev/null
+
+# HARDLINK FILE ln -P edit only in `build-web-ext`
+
+type web-ext >/dev/null 2>&1
 RET=$?
 WEBEXT_CMD='web-ext build -s ./src/ -a . --overwrite-dest'
 if [ $RET -ne 0 ];
 then
+	echo "installing web-ext .. "
 	npm install web-ext --prefix $(pwd)
 	eval ./node_modules/.bin/$WEBEXT_CMD
 else
