583680170d3dc145a87f9209f6b25c5808942026
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-06-30 14:07:58 +0200

    update buid readme and remove useless try catch

--- background.js
+++ background.js
@@ -3,38 +3,61 @@ const extId = 'tbl2csv';
 
 let hlexportables = {};
 
-function onError(e){console.log(`${extId}::onError: ${e}`);}
+function onError(e,msg){console.log(`${extId}::onError error: ${e}, stack: ${e.stack}, message: ${msg}`);}
 
 async function onUpdated(tabId, changeInfo, tabInfo) { 
 	if(typeof tabId !== 'undefined' ) {
-		browser.tabs.sendMessage(tabId, {"isOn": true}).then( () => {  // activeTab permission
-			hlexportables[tabId] = true; 
-			browser.browserAction.setBadgeText({"text": "on", "tabId": tabId}); // menus permission
-		}).catch( (err) => {
+		try {
+			await browser.tabs.sendMessage(tabId, {"isOn": true}).then( () => {  // activeTab permission
+				hlexportables[tabId] = true; 
+				browser.browserAction.setBadgeText({"text": "on", "tabId": tabId}); // menus permission
+			});
+		}catch(err) {
 			browser.browserAction.setBadgeText({"text": "", "tabId": tabId}); // menus permission
 			delete hlexportables[tabId];
-		}); 
+		}
 	}
 }
 
 async function onBrowserActionClicked(tab) { 
+
 	if(typeof tab.id !== 'undefined' ) {
 		try { 
 			await browser.tabs.executeScript({file: 'content-script.js'}); // activeTab permission
-			if(typeof hlexportables[tab.id] === 'undefined') { 
+		}catch(e){
+			onError(e, 'failed executeScript');
+			return;
+		}
+		if(typeof hlexportables[tab.id] === 'undefined') { 
 				browser.browserAction.setBadgeText({"text": "on", "tabId": tab.id}); // menu permission
 				hlexportables[tab.id] = true; 
-
-			}else { 
+		}else { 
 				browser.browserAction.setBadgeText({"text": "", "tabId": tab.id}); // menu permission
 				delete hlexportables[tab.id];
-			}
+		}
+
+		try { 
 			await browser.tabs.sendMessage(tab.id, {"hlDivTbls": true, "hlToggle": hlexportables[tab.id]}); // activeTab permission
-		}catch(e) {
-			onError(e);
+		}catch(e){
+			onError(e, 'failed sendMessage');
+			return;
 		}
 	}
 
+}
+
+async function onMenuClicked(clickData, tab) { 
+	if (typeof clickData.menuItemId === 'string' 
+		&& clickData.menuItemId.startsWith(extId)) {
+		try {
+			await browser.tabs.sendMessage(tab.id, {  // activeTab permission 
+				"targetElementId": clickData.targetElementId, 
+				"mode": clickData.menuItemId,
+			});
+		}catch(e){
+			onError(e, 'failed sendMessage');
+		}
+	}
 }
 
 ['HTML', 'TEXT'].forEach( (val) => {
@@ -46,23 +69,10 @@ async function onBrowserActionClicked(tab) {
 	});
 });
 
-async function onMenuClicked(clickData, tab) { 
-	if (typeof clickData.menuItemId === 'string' 
-		&& clickData.menuItemId.startsWith(extId)) {
-		browser.tabs.sendMessage(tab.id, {  // activeTab permission 
-			 "targetElementId": clickData.targetElementId, 
-				    "mode": clickData.menuItemId,
-		}).catch(onError);
-	}
-}
+// set Badge Background Color 
+browser.browserAction.setBadgeBackgroundColor({color: "green"}); // menu permission
+// Register Listeners 
+browser.tabs.onUpdated.addListener(onUpdated); // activeTab permission
+browser.menus.onClicked.addListener(onMenuClicked); // menu permission
+browser.browserAction.onClicked.addListener(onBrowserActionClicked); // menu permission
 
-try {
-	// set Badge Background Color 
-	browser.browserAction.setBadgeBackgroundColor({color: "green"}); // menu permission
-	// Register Listeners 
-	browser.tabs.onUpdated.addListener(onUpdated); // activeTab permission
-	browser.menus.onClicked.addListener(onMenuClicked); // menu permission
-	browser.browserAction.onClicked.addListener(onBrowserActionClicked); // menu permission
-}catch(e){
-	onError(e);
-}
